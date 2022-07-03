a121c18c9729f69e9a36adb7017d7dd0db21fbee
DaviBrinhosa <davigilvanni@gmail.com>
2022-07-03 19:20:53 -0300

    Update 001.05

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,18 @@
 const botaoMenu01 = document.querySelector('.button_01')
 const botaoMenu02 = document.querySelector('.button_02')
 const botaoMenu04 = document.querySelector('.button_04')
+const botaoMenu06 = document.querySelector('.button_06')
 const documentDisplay01 = document.querySelector('.text_01')
 const documentDisplay02 = document.querySelector('.text_02')
 const documentDisplay04 = document.querySelector('.text_04')
+const documentDisplay06 = document.querySelector('.text_06')
 
-var countToggle01 = 1
-var countToggle02 = 1
-var countToggle04 = 1
+var countToggle01 = 1, countToggle02 = 1, countToggle04 = 1, countToggle06 = 1
 
 botaoMenu01.addEventListener('click', buttonFunction01)
 botaoMenu02.addEventListener('click', buttonFunction02)
 botaoMenu04.addEventListener('click', buttonFunction04)
+botaoMenu06.addEventListener('click', buttonFunction06)
 
 function buttonFunction01() {
     if (countToggle01 % 2 !== 0) {
@@ -38,4 +39,13 @@ function buttonFunction04() {
         documentDisplay04.style.display = "none";
     }
     countToggle04++
+}
+
+function buttonFunction06() {
+    if (countToggle06 % 2 !== 0) {
+        documentDisplay06.style.display = "block";
+    } else {
+        documentDisplay06.style.display = "none";
+    }
+    countToggle06++
 }
