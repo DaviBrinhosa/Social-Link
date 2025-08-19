58e88e54395e98e7f516e9b8f98a406ede265655
Davi Brinhosa <davigilvanni@gmail.com>
2025-08-19 05:20:14 +0100

    Update 005 - 19/08/2025
    Update about the new theme, new content

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,66 +1,41 @@
-let main_title = "Espaço la vie";
+let main_title = "Lar dos Gateados";
 
-let title_01 = "Quem Somos";
-let text_01 = "Somos amigas desde 1988. E desde sempre pensávamos em trabalhar juntas. No entanto, a vida nos colocou em caminhos diferentes por muitos anos.<br><br> Nesse meio tempo, fizemos faculdade, tivemos filhos, construímos nossas carreiras (não necessariamente nessa mesma ordem), mas em caminhos distintos: Elisiane na Educação e Marcelli no Meio Ambiente. <br><br> Os filhos cresceram, a vida foi de certa forma se acalmando. Por vezes não nos entendíamos enquanto mulheres: o que queríamos, quem éramos, o que precisávamos entender. <br><br> Cada uma a sua maneira buscou o autoconhecimento, buscou a sua essência. E quis o destino que aqui a junção se concretizasse: Percebemos que, apesar de termos escolhido ferramentas diferentes para nos orientar nesse “Olhar para dentro”, elas se complementavam.<br><br> <b>Assim surge o Espaço La Vie.</b>";
-let img_01 = '<img id="img_01" class="img_01" src="./assets/img/quem-sou-marceli.png" /><img id="img_01" class="img_01" src="./assets/img/quem-sou-elisiane.png" />';
+let about_us_title = "Quem Somos";
+let about_us_text = "Somos amigas desde 1988. E desde sempre pensávamos em trabalhar juntas. No entanto, a vida nos colocou em caminhos diferentes por muitos anos.<br><br> Nesse meio tempo, fizemos faculdade, tivemos filhos, construímos nossas carreiras (não necessariamente nessa mesma ordem), mas em caminhos distintos: Elisiane na Educação e Marcelli no Meio Ambiente. <br><br> Os filhos cresceram, a vida foi de certa forma se acalmando. Por vezes não nos entendíamos enquanto mulheres: o que queríamos, quem éramos, o que precisávamos entender. <br><br> Cada uma a sua maneira buscou o autoconhecimento, buscou a sua essência. E quis o destino que aqui a junção se concretizasse: Percebemos que, apesar de termos escolhido ferramentas diferentes para nos orientar nesse “Olhar para dentro”, elas se complementavam.<br><br> <b>Assim surge o Espaço La Vie.</b>";
+let about_us_img = '<img id="img_01" class="img_01" src="./assets/img/quem-sou-elisiane.png" />';
 
-let title_02 = "Terapia Quântica Integrativa";
-let text_02 = "A <b>Terapia Quântica Integrativa</b> é o resultado da junção do conjunto de práticas e técnicas com foco nos cuidados com a saúde, considerando essa como “um estado de completo bem-estar físico, mental e social e não apenas a ausência de doença”(OMS, 1947). <br><br> O conceito de <b>Terapia Quântica Integrativa</b> representa uma perspectiva ampliada da relação entre terapeuta e cliente, visando promover uma vida com bem-estar e equilíbrio, conduzindo o indivíduo na Jornada de autoconhecimento, reduzindo ou eliminando as dificuldades existentes, ressignificando e instrumentalizando o indivíduo para que possa lidar com maior parcimônia na resolução de seus problemas físicos, mentais e emocionais. O objetivo é trabalhar o estado do 'ser', permitindo a melhora do equilíbrio entre corpo, mente e espírito, proporcionar equilíbrio e ampliação da qualidade de vida. <br><br> A <b>Terapia Quântica Integrativa</b> traz na sua essência uma visão holística que integra o corpo, as emoções e a alma da pessoa, ou seja, oferece uma visão mais ampla do ser humano nas áreas da vida.<br><br><br> <b>Agende uma sessão!</b> ";
+let our_store_title = "Nossa Loja";
+let our_store_text = "A <b>Terapia Quântica Integrativa</b> é o resultado da junção do conjunto de práticas e técnicas com foco nos cuidados com a saúde, considerando essa como “um estado de completo bem-estar físico, mental e social e não apenas a ausência de doença”(OMS, 1947). <br><br> O conceito de <b>Terapia Quântica Integrativa</b> representa uma perspectiva ampliada da relação entre terapeuta e cliente, visando promover uma vida com bem-estar e equilíbrio, conduzindo o indivíduo na Jornada de autoconhecimento, reduzindo ou eliminando as dificuldades existentes, ressignificando e instrumentalizando o indivíduo para que possa lidar com maior parcimônia na resolução de seus problemas físicos, mentais e emocionais. O objetivo é trabalhar o estado do 'ser', permitindo a melhora do equilíbrio entre corpo, mente e espírito, proporcionar equilíbrio e ampliação da qualidade de vida. <br><br> A <b>Terapia Quântica Integrativa</b> traz na sua essência uma visão holística que integra o corpo, as emoções e a alma da pessoa, ou seja, oferece uma visão mais ampla do ser humano nas áreas da vida.<br><br><br> <b>Agende uma sessão!</b> ";
 
-let title_03 = "Leitura de Tarot";
-let text_03 = "O que é tarot? <br><br> O Tarot é um jogo de 78 cartas, dividido entre 22 arcanos maiores e 56 arcanos menores. Em seu nível mais simples, o Tarot é uma variedade de símbolos arquetípicos.<br><br> O poder do Tarot é a sua capacidade de guia-la, desenvolver a sua intuição, saber o que você quer da vida e direcioná-la no caminho apontado.<br><br> Através da sincronicidade, as cartas revelam a energia a sua volta, ao mesmo tempo que trazem para fora fatos ou informações que você desconhece.<br><br> O Tarot é como um espelho, refletindo uma imagem de si mesmo no momento em que você olha pra ele.<br><br> Ele traz a tona suas esperanças e medos e aponta direções para concretiza-los ou não. Quer conhecer um pouco mais sobre o Tarot?<br><br> <b>Agende sua consulta!</b> <br><br> Ficaremos muito felizes em lhe apresentar essa maravilhosa ferramenta de autoconhecimento.<br><br> Fonte: BARTLETT, S. A Biblia do Tarot. 2021.";
+let donate_title = "Doações";
+let donate_text = "O que é tarot? <br><br> O Tarot é um jogo de 78 cartas, dividido entre 22 arcanos maiores e 56 arcanos menores. Em seu nível mais simples, o Tarot é uma variedade de símbolos arquetípicos.<br><br> O poder do Tarot é a sua capacidade de guia-la, desenvolver a sua intuição, saber o que você quer da vida e direcioná-la no caminho apontado.<br><br> Através da sincronicidade, as cartas revelam a energia a sua volta, ao mesmo tempo que trazem para fora fatos ou informações que você desconhece.<br><br> O Tarot é como um espelho, refletindo uma imagem de si mesmo no momento em que você olha pra ele.<br><br> Ele traz a tona suas esperanças e medos e aponta direções para concretiza-los ou não. Quer conhecer um pouco mais sobre o Tarot?<br><br> <b>Agende sua consulta!</b> <br><br> Ficaremos muito felizes em lhe apresentar essa maravilhosa ferramenta de autoconhecimento.<br><br> Fonte: BARTLETT, S. A Biblia do Tarot. 2021.";
 
-let title_04 = "Leitura de Oráculos";
-let text_04 = "O que são os Oráculos? <br><br> Os Oráculos são instrumentos de leitura energética, servindo como mediadores entre as mensagens que você necessita e seu eu de hoje. <br><br> Eles transmitem aprendizados e sentimentos que podem ser utilizados como guias para entender situações e dar um direcionamento sobre qual melhor caminho seguir. <br><br> Até mesmo, para colocar em evidência algo que você precisa aprender, mas não imaginava a importância. <br><br> Oráculos funcionam tendo como base influências energéticas para que, ao fazer uma consulta, você receba uma mensagem que esteia conectada com a sua própria energia naquele momento. <br><br> Os Oráculos auxiliam na promoção de autoconhecimento e nos auxiliam no processo terapêutico, revelando o que precisa tomar conhecimento e trabalhar dentro de si naquele momento. <br><br> Eles te apresentam a mensagem que seu próprio coração almeja que seja transmitida. Os Oráculos revelam seu modo de ser e estar no mundo e apontam caminhos, os quais são escolhas que devemos ou não seguir. <br><br> Quer conhecer um pouco mais sobre os Oráculos? <br><br> <b>Agende sua consulta! </b> <br><br> Ficaremos muito felizes em lhes apresentar essa maravilhosa ferramenta de autoconhecimento.";
+let castration_title = "Castração";
+let castration_text = "O que são os Oráculos? <br><br> Os Oráculos são instrumentos de leitura energética, servindo como mediadores entre as mensagens que você necessita e seu eu de hoje. <br><br> Eles transmitem aprendizados e sentimentos que podem ser utilizados como guias para entender situações e dar um direcionamento sobre qual melhor caminho seguir. <br><br> Até mesmo, para colocar em evidência algo que você precisa aprender, mas não imaginava a importância. <br><br> Oráculos funcionam tendo como base influências energéticas para que, ao fazer uma consulta, você receba uma mensagem que esteia conectada com a sua própria energia naquele momento. <br><br> Os Oráculos auxiliam na promoção de autoconhecimento e nos auxiliam no processo terapêutico, revelando o que precisa tomar conhecimento e trabalhar dentro de si naquele momento. <br><br> Eles te apresentam a mensagem que seu próprio coração almeja que seja transmitida. Os Oráculos revelam seu modo de ser e estar no mundo e apontam caminhos, os quais são escolhas que devemos ou não seguir. <br><br> Quer conhecer um pouco mais sobre os Oráculos? <br><br> <b>Agende sua consulta! </b> <br><br> Ficaremos muito felizes em lhes apresentar essa maravilhosa ferramenta de autoconhecimento.";
 
-let title_05 = "Jornada de Perséfone";
-let text_05 = "";
-
-let title_06 = "Essência Selvagem - Clube do Livro para Mulheres";
-let text_06 = "";
-
-let title_07 = "Clube do Livro - “Ser, Conviver, Transformar”";
-let text_07 = "Ao longo dos anos a vida vem me mostrando que pessoas podem fazer a diferença quando compartilham e convivem acreditando em dias melhores. <br><br> O Clube do livro surge nesta perspectiva, reunir mulheres que buscam iniciar uma jornada de autoconhecimento. Encontros que vão além das leituras dos livros indicados, encontros onde as trocas de experiências pessoais e suporte emocional são compartilhados objetivando a melhoria da convivência, a busca de alternativas e estratégias para lidar com a rotina que envolve o ser mulher. <br><br> A escolha das leituras visam auxiliar você a dar o primeiro passo para uma mudança consistente e duradoura na sua vida. Participar do clube do livro lhe oportuniza escrever um novo capítulo para a sua vida, cheio de novos insights, aprendizados e autoconhecimento.<br><br> Eu acredito que o poder da leitura está muito além de lermos uma quantidade X de livros ao ano, o verdadeiro poder está em usarmos este conhecimento e aplicá-lo transformando nossa vida.<br><br> E é exatamente esse meu desejo com sua participação no Clube do Livro “Ser, Conviver e Transformar” ao longo dos próximos meses. <br><br> Durante 6 meses estaremos juntas analisando cada leitura, dialogando e buscando trazer para nossas vivências os ensinamentos contidos nas leituras efetivadas, bem como, através do nosso convívio, instrumentalizar vocês com ferramentas práticas que lhes ajudarão na Jornada de Autoconhecimento, no tocante a melhoria de sua autoestima, maior clareza e qualificando assim suas tomada de decisões.<br><br> Chegou a hora de você SER, CONVIVER E TRANSFORMAR SUA VIDA!<br><br> <b>Vem comigo!</b>";
-
-let title_08 = "Mentoria “Seja Seu Próprio Amor”";
-let text_08 = "";
-
-let title_09 = "Oficina Escrita Terapêutica";
-let text_09 = "A escrita é essencial na nossa vida, mais do que uma forma de registro, de contar histórias e deixá-las registradas, ela é uma poderosa ferramenta que podemos utilizar na busca de autoconhecimento e para podermos entender nossos sentimentos acerca das experiências que vamos vivenciando ao longo de nossa vida. <br><br> A Oficina de Escrita Terapêutica tem por objetivo auxiliar você na Jornada de Autoconhecimento através da escrita, desbloqueando e instrumentalizando este processo, visando agregar na sua vida uma nova visão das suas experiências. <br><br> Ela acontece em dois formatos: em grupo ou individual.  Ambos os formatos são disponibilizados de forma online ou presencial. Tem duração de 1h30min. Os grupos são de no máximo 6 participantes. <br><br> Colocar seus sentimentos no papel é uma poderosa ferramenta que te auxilia na resolução de problemas, que muitas vezes represamos dentro de nós. A escrita terapêutica é uma prática que nos auxilia na busca do autoconhecimento e entender nossos sentimentos. <br><br> Este processo consiste em escrever livremente sobre os sentimentos, sem se preocupar com estrutura do texto, rimas ou regras gramaticais. O intuito é de que ela ocorra de maneira natural, sem programar o que será exposto.<br><br> Tornar a Escrita Terapêutica um hábito vai transformar sua vida!<br><br> Na oficina faremos diversos exercícios de escrita para incentivá-la a criar este hábito, bem como ajudar a refletir sobre seus sentimentos e comportamentos, utilizando os benefícios para sua evolução pessoal. <br><br> Na Oficina você dará os primeiros passos na sua jornada de autoconhecimento, ampliando sua busca de equilibrar suas emoções, vai adquirir ferramentas que auxiliam na organização dos pensamentos, melhorando sua comunicação consigo e com os que a cercam,  bem como, estabelecer um compromisso consigo mesmo que gera satisfação pessoal e auto disciplina. <br><br> O hábito da Escrita Terapêutica lhe ajudará em todas as áreas da vida dando consistência e continuidade ao seu caminho.  <br><br> <b>Venha participar!</b>";
-
-let title_10 = "Agendamento - Marceli (WhatsApp)";
-let title_11 = "Agendamento - Elisiane (WhatsApp)";
+let whatsapp_title = "Contato - Elisiane (WhatsApp)";
+let instagram_title = "Contato - Elisiane (Instagram)";
 
 let copyright = "Website developed by / Site Desenvolvido por Davi Brinhosa ©2022";
 
 document.getElementById("main_title").innerHTML = `${main_title}`;
-document.getElementById("title_01").innerHTML = `${title_01}`;
-document.getElementById("text_01").innerHTML = `${text_01}`;
-document.getElementById("img_section_01").innerHTML = `${img_01}`;
-document.getElementById("title_02").innerHTML = `${title_02}`;
-document.getElementById("text_02").innerHTML = `${text_02}`;
-document.getElementById("title_03").innerHTML = `${title_03}`;
-document.getElementById("text_03").innerHTML = `${text_03}`;
-document.getElementById("title_04").innerHTML = `${title_04}`;
-document.getElementById("text_04").innerHTML = `${text_04}`;
-document.getElementById("title_05").innerHTML = `${title_05}`;
-document.getElementById("text_05").innerHTML = `${text_05}`;
-document.getElementById("title_06").innerHTML = `${title_06}`;
-document.getElementById("text_06").innerHTML = `${text_06}`;
-document.getElementById("title_07").innerHTML = `${title_07}`;
-document.getElementById("text_07").innerHTML = `${text_07}`;
-document.getElementById("title_08").innerHTML = `${title_08}`;
-document.getElementById("text_08").innerHTML = `${text_08}`;
-document.getElementById("title_09").innerHTML = `${title_09}`;
-document.getElementById("text_09").innerHTML = `${text_09}`;
-document.getElementById("title_10").innerHTML = `${title_10}`;
-document.getElementById("title_11").innerHTML = `${title_11}`;
+document.getElementById("about_us_title").innerHTML = `${about_us_title}`;
+document.getElementById("about_us_text").innerHTML = `${about_us_text}`;
+document.getElementById("about_us_img").innerHTML = `${about_us_img}`;
+document.getElementById("our_store_title").innerHTML = `${our_store_title}`;
+document.getElementById("our_store_text").innerHTML = `${our_store_text}`;
+document.getElementById("donate_title").innerHTML = `${donate_title}`;
+document.getElementById("donate_text").innerHTML = `${donate_text}`;
+document.getElementById("castration_title").innerHTML = `${castration_title}`;
+document.getElementById("castration_text").innerHTML = `${castration_text}`;
+document.getElementById("whatsapp_title").innerHTML = `${whatsapp_title}`;
+document.getElementById("instagram_title").innerHTML = `${instagram_title}`;
 document.getElementById("copyright").innerHTML = `${copyright}`;
 
-document.getElementById("button_10").onclick  = function() {
-    window.open('https://wa.me/5548992033182','_blank');
+document.getElementById("whatsapp_button").onclick  = function() {
+    window.open('https://wa.me/554891178383','_blank');
 }
 
-document.getElementById("button_11").onclick  = function() {
-    window.open('https://wa.me/5548991782760','_blank');
+document.getElementById("instagram_button").onclick  = function() {
+    window.open('https://www.instagram.com/lar_dos_gateados/','_blank');
 }
